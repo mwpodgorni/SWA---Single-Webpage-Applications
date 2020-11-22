7322f11ab8e5fc3ec27758356c191e48e89cbebf
mwpodgorni <mwpodgorni@gmail.com>
2020-11-22 18:22:04 +0100

    Assignment
    corrections exercise 4.2

--- Assignments/Assignment 4/Exercise 4.2/index.js
+++ Assignments/Assignment 4/Exercise 4.2/index.js
@@ -2,14 +2,11 @@ const http = require("http");
 const express = require("express");
 const path = require("path");
 const app = express();
-const fetch = require("node-fetch");
 
 app.use(express.json());
 app.use(express.static("express"));
-// default URL for website
 app.use("/", function (req, res) {
   res.sendFile(path.join(__dirname + "/index.html"));
-  //__dirname : It will resolve to your project folder.
 });
 const server = http.createServer(app);
 const port = 3000;
