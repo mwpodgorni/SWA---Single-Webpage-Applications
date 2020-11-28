dba7d880c29f7e0199119911f057b307e8be5bfa
Nikita <266900@via.dk>
2020-11-28 17:25:38 +0100

    rxjs poling . getting updated data

--- Assignments/Assignment 4/Exercise4.1/index.js
+++ Assignments/Assignment 4/Exercise4.1/index.js
@@ -6,12 +6,10 @@ const app = express();
 const router = express.Router();
 
 
-
 app.use(express.json());
 app.use(express.static("express"));
 
 
-// app.use(express.static(path.join(__dirname + '/js/myjsfile.js')))
 
 app.use("/", function (req, res) {
   res.sendFile(path.join(__dirname + "/index.html"));
@@ -19,24 +17,8 @@ app.use("/", function (req, res) {
 );
 
 
-// router.get('/js',function(req,res){
-//   res.sendFile(path.join(__dirname+'/about.html'));
-// });
-
 const server = http.createServer(app);
 const port = 3030;
 server.listen(port);
 console.debug("Server listening on port " + port);
 
-
-
-// console.log ("this is aaaa");
-
-// const poll_url = url => interval(100).pipe(concatMap(() => ajax.getJSON(url)));
-
-// const poll_something = () => 
-//       poll_url('http://localhost:8080/warnings')
-//       // .pipe (map(res => console.log ('1)  ', res)));
-
-//       // console.log(map)
-//       console.log ("aaaa   ",  poll_something());
